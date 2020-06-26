16ea591b7adf5fab0416eeaeb7bd944db4289bec
Murthy Guruswamy <mur3kittu@gmail.com>
2020-06-25 21:22:47 -0400

    Login with social icons

---
+++ src/components/login/index.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import {MDBIcon, MDBTooltip} from 'mdbreact';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+import {GrGoogle, GrFacebookOption} from 'react-icons/gr';
+
+class LoginFormComponent extends Component {
+  state = {
+    name: '',
+  };
+  responseFacebook = (response) => {
+    console.log(response);
+    this.setState({name: response.name});
+  };
+
+  responseGoogle = (response) => {
+    console.log(response);
+  };
+
+  render() {
+    return (
+      <>
+        <div
+          className="modal fade"
+          id="modalLRForm"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="myModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog cascading-modal" role="document">
+            <div className="modal-content">
+              <div class="modal-header light-blue darken-3 white-text">
+                <h4 class="title">
+                  <i class="fas fa-users"></i> Login with your Social Network
+                </h4>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body mb-0  text-center">
+                <FacebookLogin
+                  appId={process.env.facebookAppId}
+                  fields="name,email,picture"
+                  icon={<GrFacebookOption />}
+                  callback={this.responseFacebook}
+                  textButton=""
+                />
+                &nbsp;&nbsp; &nbsp;
+                <GoogleLogin
+                  clientId={process.env.googleClientId}
+                  buttonText=""
+                  icon={<GrGoogle />}
+                  onSuccess={this.responseGoogle}
+                  onFailure={this.responseGoogle}
+                />
+                <div>Logged in as {this.state.name}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <MDBIcon icon="user" data-toggle="modal" data-target="#modalLRForm" />
+        </div>
+      </>
+    );
+  }
+}
+
+export default LoginFormComponent;
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import {
   MDBNavbar,
@@ -12,6 +12,7 @@ import {
   MDBDropdownToggle,
   MDBIcon,
 } from 'mdbreact';
+import LoginFormComponent from './components/login';
 
 class Routes extends React.Component {
   constructor(props) {
@@ -26,223 +27,6 @@ class Routes extends React.Component {
     this.setState({isOpen: !this.state.isOpen});
   };
 
-  loginForm = () => {
-    return (
-      <>
-        <div
-          className="modal fade"
-          id="modalLRForm"
-          tabIndex="-1"
-          role="dialog"
-          aria-labelledby="myModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog cascading-modal" role="document">
-            <div className="modal-content">
-              <div className="modal-c-tabs">
-                <ul
-                  className="nav nav-tabs md-tabs tabs-2 light-blue darken-3"
-                  role="tablist"
-                >
-                  <li className="nav-item">
-                    <a
-                      className="nav-link active"
-                      data-toggle="tab"
-                      href="#panel7"
-                      role="tab"
-                    >
-                      <i className="fas fa-user mr-1"></i>
-                      Login
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a
-                      className="nav-link"
-                      data-toggle="tab"
-                      href="#panel8"
-                      role="tab"
-                    >
-                      <i className="fas fa-user-plus mr-1"></i>
-                      Register
-                    </a>
-                  </li>
-                </ul>
-
-                <div className="tab-content">
-                  <div
-                    className="tab-pane fade in show active"
-                    id="panel7"
-                    role="tabpanel"
-                  >
-                    <div className="modal-body mb-1">
-                      <div className="row my-3 d-flex justify-content-center">
-                        <div className="md-form form-sm mb-5">
-                          <button
-                            type="button"
-                            className="btn btn-white btn-rounded mr-md-3 z-depth-1a"
-                          >
-                            <i className="fab fa-facebook-f text-center"></i>
-                          </button>
-                          <button
-                            type="button"
-                            className="btn btn-white btn-rounded z-depth-1a"
-                            onClick={this.prepareLoginButton}
-                            ref="googleLoginBtn"
-                          >
-                            <i className="fab fa-google-plus-g"></i>
-                          </button>
-                        </div>
-                      </div>
-                      <div className="md-form form-sm mb-5">
-                        <i className="fas fa-envelope prefix"></i>
-                        <input
-                          type="email"
-                          id="modalLRInput10"
-                          className="form-control form-control-sm validate"
-                        />
-                        <label
-                          data-error="wrong"
-                          data-success="right"
-                          htmlFor="modalLRInput10"
-                        >
-                          Your email
-                        </label>
-                      </div>
-
-                      <div className="md-form form-sm mb-4">
-                        <i className="fas fa-lock prefix"></i>
-                        <input
-                          type="password"
-                          id="modalLRInput11"
-                          className="form-control form-control-sm validate"
-                        />
-                        <label
-                          data-error="wrong"
-                          data-success="right"
-                          htmlFor="modalLRInput11"
-                        >
-                          Your password
-                        </label>
-                      </div>
-                      <div className="text-center mt-2">
-                        <button className="btn btn-info">
-                          Log in <i className="fas fa-sign-in ml-1"></i>
-                        </button>
-                      </div>
-                    </div>
-
-                    <div className="modal-footer">
-                      <div className="options text-center text-md-right mt-1">
-                        <p>
-                          Not a member?{' '}
-                          <a href="#" className="blue-text">
-                            Sign Up
-                          </a>
-                        </p>
-                        <p>
-                          Forgot{' '}
-                          <a href="#" className="blue-text">
-                            Password?
-                          </a>
-                        </p>
-                      </div>
-                      <button
-                        type="button"
-                        className="btn btn-outline-info waves-effect ml-auto"
-                        data-dismiss="modal"
-                      >
-                        Close
-                      </button>
-                    </div>
-                  </div>
-
-                  <div className="tab-pane fade" id="panel8" role="tabpanel">
-                    <div className="modal-body">
-                      <div className="md-form form-sm mb-5">
-                        <i className="fas fa-envelope prefix"></i>
-                        <input
-                          type="email"
-                          id="modalLRInput12"
-                          className="form-control form-control-sm validate"
-                        />
-                        <label
-                          data-error="wrong"
-                          data-success="right"
-                          htmlFor="modalLRInput12"
-                        >
-                          Your email
-                        </label>
-                      </div>
-
-                      <div className="md-form form-sm mb-5">
-                        <i className="fas fa-lock prefix"></i>
-                        <input
-                          type="password"
-                          id="modalLRInput13"
-                          className="form-control form-control-sm validate"
-                        />
-                        <label
-                          data-error="wrong"
-                          data-success="right"
-                          htmlFor="modalLRInput13"
-                        >
-                          Your password
-                        </label>
-                      </div>
-
-                      <div className="md-form form-sm mb-4">
-                        <i className="fas fa-lock prefix"></i>
-                        <input
-                          type="password"
-                          id="modalLRInput14"
-                          className="form-control form-control-sm validate"
-                        />
-                        <label
-                          data-error="wrong"
-                          data-success="right"
-                          htmlFor="modalLRInput14"
-                        >
-                          Repeat password
-                        </label>
-                      </div>
-
-                      <div className="text-center form-sm mt-2">
-                        <button className="btn btn-info">
-                          Sign up <i className="fas fa-sign-in ml-1"></i>
-                        </button>
-                      </div>
-                    </div>
-
-                    <div className="modal-footer">
-                      <div className="options text-right">
-                        <p className="pt-1">
-                          Already have an account?{' '}
-                          <a href="#" className="blue-text">
-                            Log In
-                          </a>
-                        </p>
-                      </div>
-                      <button
-                        type="button"
-                        className="btn btn-outline-info waves-effect ml-auto"
-                        data-dismiss="modal"
-                      >
-                        Close
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="text-center">
-          <MDBIcon icon="user" data-toggle="modal" data-target="#modalLRForm" />
-        </div>
-      </>
-    );
-  };
   render() {
     return (
       <MDBNavbar color="default-color" dark expand="md">
@@ -269,7 +53,9 @@ class Routes extends React.Component {
           <MDBNavbarNav right>
             <MDBNavItem>
               <MDBDropdown>
-                <MDBDropdownToggle nav>{this.loginForm()}</MDBDropdownToggle>
+                <MDBDropdownToggle nav>
+                  <LoginFormComponent />
+                </MDBDropdownToggle>
               </MDBDropdown>
             </MDBNavItem>
           </MDBNavbarNav>
